7bfff10de8a6d09d735669cec641947a809481a5
fabohax <lefabport@gmail.com>
2020-05-29 01:45:26 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $("#contacto").click(function(){
+	    console.log("clicked");
+	    $("front").animate({
+		    	width: '100%'
+		  	}
+	    );
+	    $("contacto").css({
+	    	display: 'block'
+	    });
+	    $("contacto").animate({
+	    	opacity: '1'
+	    });
+	    $("logo img").css({
+	    	filter: 'invert(1)'
+	    })
+  	});
+
+});
