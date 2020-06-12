a0325e0778c86684d4bea09727d9bb639ca3de22
fabohax <lefabport@gmail.com>
2020-06-12 17:58:28 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 
   	function loop(){
 
-  		t1 = 2500;
+  		t1 = 1500;
   		t2 = t1*2;
   		t3 = t1*3;
   		t4 = t1*4;
@@ -26,43 +26,43 @@ $(document).ready(function(){
 
 
   		$("mouth img#1").animate({opacity: '0',display:'block'}, t1);
-	  	$("mouth img#1").animate({opacity: '1'}, t1);
-	  	$("mouth img#1").animate({opacity: '1'}, t1);
+	  	$("mouth img#1").animate({opacity: '1'}, t2);
+	  	$("mouth img#1").animate({opacity: '1'}, t2);
 	  	$("mouth img#1").animate({opacity: '0',display:'none'}, t1);
 
-	  	$("mouth img#2").animate({opacity: '0',display:'block'}, t1*5);
-	  	$("mouth img#2").animate({opacity: '1'}, t1);
-	  	$("mouth img#2").animate({opacity: '1'}, t1);
+	  	$("mouth img#2").animate({opacity: '0',display:'block'}, t1*6);
+	  	$("mouth img#2").animate({opacity: '1'}, t2);
+	  	$("mouth img#2").animate({opacity: '1'}, t2);
 	  	$("mouth img#2").animate({opacity: '0',display:'none'}, t1);
 
-	  	$("mouth img#3").animate({opacity: '0',display:'block'}, t1*8);
-	  	$("mouth img#3").animate({opacity: '1'}, t1);
-	  	$("mouth img#3").animate({opacity: '1'}, t1);
+	  	$("mouth img#3").animate({opacity: '0',display:'block'}, t1*11);
+	  	$("mouth img#3").animate({opacity: '1'}, t2);
+	  	$("mouth img#3").animate({opacity: '1'}, t2);
 	  	$("mouth img#3").animate({opacity: '0',display:'none'}, t1);
 
-	  	$("mouth img#4").animate({opacity: '0',display:'block'}, t1*11);
-	  	$("mouth img#4").animate({opacity: '1'}, t1);
-	  	$("mouth img#4").animate({opacity: '1'}, t1);
+	  	$("mouth img#4").animate({opacity: '0',display:'block'}, t1*16);
+	  	$("mouth img#4").animate({opacity: '1'}, t2);
+	  	$("mouth img#4").animate({opacity: '1'}, t2);
 	  	$("mouth img#4").animate({opacity: '0',display:'none'}, t1);
 
-	  	$("mouth img#5").animate({opacity: '0',display:'block'}, t1*14);
-	  	$("mouth img#5").animate({opacity: '1'}, t1);
-	  	$("mouth img#5").animate({opacity: '1'}, t1);
+	  	$("mouth img#5").animate({opacity: '0',display:'block'}, t1*21);
+	  	$("mouth img#5").animate({opacity: '1'}, t2);
+	  	$("mouth img#5").animate({opacity: '1'}, t2);
 	  	$("mouth img#5").animate({opacity: '0',display:'none'}, t1);
 
-	  	$("mouth img#6").animate({opacity: '0',display:'block'}, t1*17);
-	  	$("mouth img#6").animate({opacity: '1'}, t1);
-	  	$("mouth img#6").animate({opacity: '1'}, t1);
+	  	$("mouth img#6").animate({opacity: '0',display:'block'}, t1*26);
+	  	$("mouth img#6").animate({opacity: '1'}, t2);
+	  	$("mouth img#6").animate({opacity: '1'}, t2);
 	  	$("mouth img#6").animate({opacity: '0',display:'none'}, t1);
 
-	  	$("mouth img#7").animate({opacity: '0',display:'block'}, t1*20);
-	  	$("mouth img#7").animate({opacity: '1'}, t1);
-	  	$("mouth img#7").animate({opacity: '1'}, t1);
+	  	$("mouth img#7").animate({opacity: '0',display:'block'}, t1*31);
+	  	$("mouth img#7").animate({opacity: '1'}, t2);
+	  	$("mouth img#7").animate({opacity: '1'}, t2);
 	  	$("mouth img#7").animate({opacity: '0',display:'none'}, t1);
 
-	  	$("mouth img#8").animate({opacity: '0',display:'block'}, t1*23);
-	  	$("mouth img#8").animate({opacity: '1'}, t1);
-	  	$("mouth img#8").animate({opacity: '1'}, t1);
+	  	$("mouth img#8").animate({opacity: '0',display:'block'}, t1*36);
+	  	$("mouth img#8").animate({opacity: '1'}, t2);
+	  	$("mouth img#8").animate({opacity: '1'}, t2);
 	  	$("mouth img#8").animate({opacity: '0',display:'none'}, t1);
 
   	}
